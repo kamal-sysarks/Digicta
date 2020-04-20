5605108197acc657ee492dfd2675a5c069a82d8f
AatreshKulkarni <aatresh.kulkarni08@gmail.com>
2020-04-20 13:17:58 +0530

    Delete Node Server

--- nodeserver/index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const db = require("mongodb");
-const app = express();
-
-
-app.listen(3000, () => {
-    console.log("Server up and running on port 3000");
-});
